d69192e3927f298c6f7898040b070f2773753d02
Linwitness <58009812+Linwitness@users.noreply.github.com>
2019-11-25 19:01:55 -0500

    Basic version of all functions
    All the sql queries of basic funcitons are finished, but there are still some little parts need to be fixed to fit the html pages.

--- NBA-Database/src/Database.java
+++ NBA-Database/src/Database.java
@@ -42,7 +42,7 @@ public class Database {
         return teamNames;
     }
     
-    // Get team rank query
+    // Get team's rank in 2012
     public ArrayList<String> getTeamranks() throws SQLException {
         String sql = "SELECT team_abbreviation, count(team_result) as teamWin\r\n" + 
         		"FROM game\r\n" + 
@@ -65,7 +65,7 @@ public class Database {
         return teamNames;
     }
     
-    //get player's rank query
+    //Get player's rank in 2012
     public ArrayList<String> getPlayerranks() throws SQLException {
         String sql = "SELECT player_name, play_position, round(sum(playast)/\r\n" + 
         		"    (SELECT max(sum(playast)) \r\n" + 
@@ -97,7 +97,8 @@ public class Database {
         return playerNames;
     } 
     
-    // Get the MVP of each match
+    // Get the MVP of each match, the example match at 2012-10-30-20:00 MIA VS ??, 
+    //the match information should change with html input 
     public ArrayList<String> getMVP() throws SQLException {
         String sql = "SELECT *\r\n" + 
         		"FROM\r\n" + 
@@ -126,7 +127,9 @@ public class Database {
         return MVP;
     } 
     
-    // Get the improvement of each player
+    // Get the improvement of each player, the example player is 'LeBron James';
+    // Input changes it;
+    // to achieve that, a new view Improvement is created.
     public ArrayList<String> getPlayerimpro() throws SQLException {
         String sql = "SELECT seasonyear, \r\n" + 
         		"    round(avg(playpts),3) as average_point, \r\n" + 
@@ -151,6 +154,80 @@ public class Database {
         return impro;
     }
 
+    // Get the winning percentage of first team in history, the example teams are 'TOR' and 'BKN'
+    // Input can change the two teams.
+    public ArrayList<String> getwinpercen() throws SQLException {
+        String sql = "SELECT team_abbreviation, \r\n" + 
+        		"    round(count(team_result)/(SELECT count(team_result) \r\n" + 
+        		"                              FROM game \r\n" + 
+        		"                              WHERE team_abbreviation = 'TOR' \r\n" + 				// TEAM1 can be changed
+        		"                              AND oppose_abbreviation = 'BKN'), 3) as HIS_WIN_PERCENTAGE\r\n" + //TEAM2 can be changed
+        		"FROM game\r\n" + 
+        		"WHERE team_abbreviation = 'TOR'\r\n" + 
+        		"    AND oppose_abbreviation = 'BKN'\r\n" + 
+        		"    AND team_result = 'Win'\r\n" + 
+        		"GROUP BY team_abbreviation";
+        ArrayList<String> winpercen = new ArrayList<String>();
+        try (Connection connection = DriverManager.getConnection(databaseURL, user, password)) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+            	winpercen.add(result.getString("HIS_WIN_PERCENTAGE"));
+            }
+             
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }      
+        return winpercen;
+    }
+    
+    // Get the Cooperation of each team
+    // A new table player_in_team is created to finish query Cooperation and Team Style
+    public ArrayList<String> getCooperation() throws SQLException {
+        String sql = "SELECT distinct team_abbreviation, round(teamwinscore - AVG(rank_score), 3) as Cooperation_rank\r\n" + 
+        		"FROM team_rank natural join player_in_team natural join player_rank\r\n" + 
+        		"GROUP BY team_abbreviation, teamwinscore\r\n" + 
+        		"ORDER BY Cooperation_rank desc";
+        ArrayList<String> Cooperation = new ArrayList<String>();
+        try (Connection connection = DriverManager.getConnection(databaseURL, user, password)) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+            	Cooperation.add(result.getString("TEAM_ABBREVIATION")+ "" + result.getString("COOPERATION_RANK"));
+            }
+             
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }      
+        return Cooperation;
+    }
+    
+    // Get figure of the team style of each team, the example team is 'CLE';
+    // Two new view TEAM_RANK and PLAYER_RANK are created in this part job
+    public ArrayList<String> getTeamstyle() throws SQLException {
+        String sql = "SELECT game_date, game_time, team_abbreviation, ROUND(avg(playorb), 3) AS OFFENSIVE,\r\n" + 
+        		"    ROUND(avg(playdrb), 3) AS DEFENSIVE, ROUND(avg(playorb)/(avg(playdrb)+0.001), 2) AS OFFENSIVILITY\r\n" + 
+        		"FROM player_in_team\r\n" + 
+        		"WHERE team_abbreviation = 'CLE'\r\n" +    //The team_abbreviation can be changed
+        		"GROUP BY game_date, game_time, team_abbreviation\r\n" + 
+        		"ORDER BY game_date, game_time";
+        ArrayList<String> Teamstyle = new ArrayList<String>();
+        try (Connection connection = DriverManager.getConnection(databaseURL, user, password)) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+            	Teamstyle.add(result.getString("GAME_DATE")+ "" + result.getString("GAME_TIME") + "" + result.getString("TEAM_ABBREVIATION") + "" + result.getString("OFFENSIVILITY"));
+            }
+             
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }      
+        return Teamstyle;
+    }
+    
     
     
     public ArrayList<String> getGames() throws SQLException {
